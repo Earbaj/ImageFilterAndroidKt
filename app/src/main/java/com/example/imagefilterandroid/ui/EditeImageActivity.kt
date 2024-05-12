a1afe9778ec2938eab862ce40ea25bcb8699a4fa
earba <earbajsaria3@gmail.com>
2024-05-12 17:31:27 +0600

    design edit image layout and create custome layout for image filter

---
+++ app/src/main/java/com/example/imagefilterandroid/ui/EditeImageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.imagefilterandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imagefilterandroid.R
+
+class EditeImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edite_image)
+    }
+}
