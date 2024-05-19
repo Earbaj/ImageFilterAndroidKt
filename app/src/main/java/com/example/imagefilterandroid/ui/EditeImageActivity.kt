c35012c1ad1fe7a5fefaea2371f41c6c74437cc6
earba <earbajsaria3@gmail.com>
2024-05-19 15:11:12 +0600

    now add methode for edit new image button so that it can pick image from file and navigate to edite image activity

--- app/src/main/java/com/example/imagefilterandroid/ui/EditeImageActivity.kt
+++ app/src/main/java/com/example/imagefilterandroid/ui/EditeImageActivity.kt
@@ -3,8 +3,11 @@ package com.example.imagefilterandroid.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imagefilterandroid.R
+import com.example.imagefilterandroid.databinding.ActivityEditeImageBinding
 
 class EditeImageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditeImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edite_image)
