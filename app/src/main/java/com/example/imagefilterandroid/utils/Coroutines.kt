163a15b6da1e75c34b3c96bee95e56d0a32cf02a
earba <earbajsaria3@gmail.com>
2024-06-30 12:57:43 +0600

    add coroutine class to project

---
+++ app/src/main/java/com/example/imagefilterandroid/utils/Coroutines.kt
@@ -0,0 +1,14 @@
+package com.example.imagefilterandroid.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+object Coroutines {
+
+    fun io(work: suspend (() -> Unit)){
+        CoroutineScope(Dispatchers.IO).launch { work() }
+    }
+
+
+}
