b582b102339a5d06ff88fb4da3703a844479d0d1
earbaj <earbajsaria3@gmail.com>
2024-11-10 15:52:51 -0800

    apply filter effect to image

---
+++ app/src/main/java/com/example/imagefilterandroid/listeners/ImageFilterListener.kt
@@ -0,0 +1,7 @@
+package com.example.imagefilterandroid.listeners
+
+import com.example.imagefilterandroid.data.ImageFilter
+
+interface ImageFilterListener {
+    fun onFilterSelected(imageFilter: ImageFilter)
+}
